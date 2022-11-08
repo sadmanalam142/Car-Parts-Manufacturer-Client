7eb4f5b9f07a8f2a6f83cc6630336dd7ec081152
sadmanalam142 <sadmanalam142@gmail.com>
2022-11-08 18:10:03 +0600

    banner styled

--- src/Pages/Home/Banner/Banner.js
+++ src/Pages/Home/Banner/Banner.js
@@ -14,26 +14,26 @@ const Banner = () => {
     return (
         <div className="hero min-h-screen bg-base-200">
             <div className="hero-content flex-col lg:flex-row-reverse">
-                <div className="carousel w-full">
+                <div className="carousel w-3/4">
                     <div id="slide1" className="carousel-item relative w-full">
                         <img src={banner1} className="w-full h-96 border-0 rounded-lg"></img>
                         <div className="absolute flex justify-between transform -translate-y-1/2 left-5 right-5 top-1/2">
-                            <a href="#slide4" className="btn btn-circle">❮</a>
-                            <a href="#slide2" className="btn btn-circle">❯</a>
+                            <a href="#slide3" className="text-secondary btn btn-ghost">❮</a>
+                            <a href="#slide2" className="text-secondary btn btn-ghost">❯</a>
                         </div>
                     </div>
                     <div id="slide2" className="carousel-item relative w-full">
                         <img src={banner2} className="w-full h-96 border-0 rounded-lg"></img>
                         <div className="absolute flex justify-between transform -translate-y-1/2 left-5 right-5 top-1/2">
-                            <a href="#slide1" className="btn btn-circle">❮</a>
-                            <a href="#slide3" className="btn btn-circle">❯</a>
+                            <a href="#slide1" className="text-secondary btn btn-ghost">❮</a>
+                            <a href="#slide3" className="text-secondary btn btn-ghost">❯</a>
                         </div>
                     </div>
                     <div id="slide3" className="carousel-item relative w-full">
                         <img src={banner3} className="w-full h-96 border-0 rounded-lg"></img>
                         <div className="absolute flex justify-between transform -translate-y-1/2 left-5 right-5 top-1/2">
-                            <a href="#slide2" className="btn btn-circle">❮</a>
-                            <a href="#slide1" className="btn btn-circle">❯</a>
+                            <a href="#slide2" className="text-secondary btn btn-ghost">❮</a>
+                            <a href="#slide1" className="text-secondary btn btn-ghost">❯</a>
                         </div>
                     </div>
                 </div>
